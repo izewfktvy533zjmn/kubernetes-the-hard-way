7ed807641d4ad02c3af80151072e5c6967cb2e60
相馬 大知 (Taichi Soma) <47720072+sosomasox@users.noreply.github.com>
2021-04-04 11:03:12 +0900

    Update and rename node-role4master.txt to apply_node-role4master.sh

--- apply_node-role4master.sh
+++ apply_node-role4master.sh
@@ -1,3 +1,8 @@
+#!/bin/bash
+
 kubectl label node `hostname` node-role.kubernetes.io/master=''
 kubectl label node `hostname` node-role.kubernetes.io/control-plane=''
+kubectl label node `hostname` node-role.kubernetes.io/etcd=''
 kubectl taint node `hostname` node-role.kubernetes.io/master=:NoSchedule
+
+exit 0
